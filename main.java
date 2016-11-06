36d392d693ed492a12a2dc1f7250735c12411e84
lukeclement <lclement.57468@farnborough.ac.uk>
2016-11-06 21:33:11 +0000

    Expanded stuff again

--- main.java
+++ main.java
@@ -5,15 +5,45 @@ public class main{
     public static void main(String[] args){
         List<String> links = new ArrayList<>();
         links.add("Main_Page");
+        int show=1;
+        int record=1;
+        String fileName="links.txt";
+        String fileNameTwo="number.txt";
+        String lining=null;
+        int current=0;
+        try{
+          FileReader fileReader = new FileReader(fileName);
+          BufferedReader br=new BufferedReader(fileReader);
+          while((lining=br.readLine())!=null){
+            links.add(lining);
+          }
+          br.close();
+        }catch(Exception ex){}
+        try{
+          FileReader fileReaderr = new FileReader(fileNameTwo);
+          BufferedReader brr=new BufferedReader(fileReaderr);
+          while((lining=brr.readLine())!=null){
+            current=Integer.parseInt(lining);
+          }
+          brr.close();
+        }catch(Exception ex){}
+
         //Node origin=new Node("Main_Page");
         List<Integer> topLinks=new ArrayList<>();
-        for(int po=0;po<links.size();po++){
+        System.out.println("Found "+links.size()+" starting at "+current);
+        for(int po=current;po<links.size();po++){
               URL url;
               InputStream is=null;
               BufferedReader br;
               String line;
               List<String> lines=new ArrayList<>();
               int linksadded=0;
+              try{
+                FileWriter writer = new FileWriter(fileNameTwo, false);
+                PrintWriter print_liner = new PrintWriter(writer);
+                print_liner.printf( "%s" + "%n" , po);
+                print_liner.close();
+              }catch(Exception ex){}
               try {
                   url = new URL("https://en.wikipedia.org/wiki/"+links.get(po));
                   is = url.openStream();  // throws an IOException
@@ -42,6 +72,7 @@ public class main{
               &&!lines.get(i).substring(j+15,j+23).equals("Special:")
               &&!lines.get(i).substring(j+15,j+20).equals("Help:")
               &&!lines.get(i).substring(j+15,j+20).equals("Talk:")
+              &&!lines.get(i).substring(j+15,j+20).equals("User:")
               &&!lines.get(i).substring(j+15,j+22).equals("Portal:")
               &&!lines.get(i).substring(j+15,j+24).equals("Category:")
               &&!lines.get(i).substring(j+15,j+25).equals("Wikipedia:")){
@@ -50,7 +81,18 @@ public class main{
                   end++;
                 }
                 if(!links.contains(lines.get(i).substring(j+15,end))){
+                  if(show==record){
+                    record=show*2;
+                    //System.out.println("Found "+show+" entries, currently searching link number "+po+" ("+links.get(po)+")");
+                  }
+                  show++;
                   links.add(lines.get(i).substring(j+15,end));
+                  try{
+                    FileWriter write = new FileWriter(fileName, true);
+                    PrintWriter print_line = new PrintWriter(write);
+                    print_line.printf( "%s" + "%n" , lines.get(i).substring(j+15,end));
+                    print_line.close();
+                  }catch(Exception ex){}
                   linksadded++;
                   //origin.addEdge(new Node(links.get(1)));
                   //System.out.println("Link to "+lines.get(i).substring(j+15,end));
@@ -66,8 +108,9 @@ public class main{
             }
           }
           if(test){
-            System.out.println(links.get(po)+" has "+linksadded+" links (searched "+((float)po/(float)links.size())+"%)");
+            System.out.println(links.get(po)+" has "+linksadded+" links (searched "+((float)po*100/(float)links.size())+"%)");
           }
+
         }
     }
 }
